31fb400405044bec876a545321f047233af49d85
rogerio75 <rogerio.ifg@gmail.com>
2023-10-11 17:30:12 -0300

    ini

---
+++ src/app/pages/sobre/sobre.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrls: ['./sobre.component.scss']
+})
+export class SobreComponent implements OnInit {
+
+constructor  (ActivatedRoute:ActivatedRoute){
+
+
+}
+ngOnInit(): void {
+  this.ActivatedRoute.params.subscribe(
+     res => console.log(res.id, res.username)
+     error => error
+
+  )
+}
+
+
+}
